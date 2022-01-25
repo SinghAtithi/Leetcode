838a81a89815f03ecb482df2d8634f18f059baeb
Atithi Kumar singh <mails4rockstararyan@gmail.com>
2022-01-26 00:03:07 +0530

    Time: 236 ms (61.93%), Space: 14 MB (81.29%) - LeetHub

---
+++ 1690-stone-game-vii/1690-stone-game-vii.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int n;
+    bool flag;
+    int dp[1001][1001];
+    int r(int i, int j, vector<int>&v, int sum){
+        if(j<i)return 0;
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int ans;
+        if((j-i+1)%2==(n%2)){
+            int q=abs(sum-v[i]-r(i+1,j,v,sum-v[i]));
+            int qq=abs(sum-v[j]-r(i,j-1,v,sum-v[j]));
+            ans=max(q,qq);
+        }
+        else{
+            int q=abs(sum-v[i]-r(i+1,j,v,sum-v[i]));
+            int qq=abs(sum-v[j]-r(i,j-1,v,sum-v[j]));
+            ans=max(q,qq);
+            // ans*=-1;
+        }
+        // cout<<ans<<" ";
+        return dp[i][j]= ans;
+        
+    }
+    
+    int stoneGameVII(vector<int>& stones) {
+        n=stones.size(); 
+        for(int i=0;i<1001;i++){
+            for(int j=0;j<1001;j++){
+                dp[i][j]=-1;
+            }
+        }
+        int sum=0;
+        for(auto &x:stones){
+            sum+=x;
+        }
+        int ans=r(0,n-1,stones,sum);
+        return ans;
+    }
+};
