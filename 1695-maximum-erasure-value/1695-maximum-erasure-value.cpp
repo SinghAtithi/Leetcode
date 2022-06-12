8ef52a2988424cece4a3bf27b54c20a666c6de37
Atithi Kumar singh <mails4rockstararyan@gmail.com>
2022-06-12 12:35:51 +0530

    Time: 374 ms (73.51%), Space: 99.7 MB (74.36%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
-        
+        ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
         int ans = 0;
         int i = 0, j = 0;
         int curr = 0;
