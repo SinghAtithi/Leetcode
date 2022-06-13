5a8bf40d84b2cab02ac40809108f1be557af9c1a
Atithi Kumar singh <mails4rockstararyan@gmail.com>
2022-06-13 14:43:58 +0530

    Time: 16 ms (11.39%), Space: 8.5 MB (91.19%) - LeetHub

---
+++ 120-triangle/120-triangle.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+        
+        int ans;
+        int row = triangle.size();
+        
+        if(row == 1){
+            return triangle[0][0];
+        }
+        
+        for(int i = 1; i < row; i++){
+            int temp = INT_MAX;
+            ans = INT_MAX;
+            for(int j = 0; j <= i; j++){
+                int x = INT_MAX;
+                if(j != 0){
+                    x = min(x, triangle[i-1][j-1]);
+                }
+                if(j != i){
+                    x = min(x, triangle[i-1][j]);
+                }
+                triangle[i][j] += x;
+                ans = min(ans,triangle[i][j]);
+            }
+        }
+        return ans;
+    }
+};
