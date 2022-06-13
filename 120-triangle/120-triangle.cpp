60149d9cce8805363466c0070afffd517fc8340a
Atithi Kumar singh <mails4rockstararyan@gmail.com>
2022-06-13 14:45:48 +0530

    Time: 8 ms (61.32%), Space: 8.7 MB (70.75%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -4,28 +4,12 @@ public:
         
         ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
         
-        int ans;
-        int row = triangle.size();
-        
-        if(row == 1){
-            return triangle[0][0];
-        }
-        
-        for(int i = 1; i < row; i++){
-            int temp = INT_MAX;
-            ans = INT_MAX;
-            for(int j = 0; j <= i; j++){
-                int x = INT_MAX;
-                if(j != 0){
-                    x = min(x, triangle[i-1][j-1]);
-                }
-                if(j != i){
-                    x = min(x, triangle[i-1][j]);
-                }
-                triangle[i][j] += x;
-                ans = min(ans,triangle[i][j]);
+        int n=triangle.size();
+        for(int i=n-2;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                triangle[i][j]+=min(triangle[i+1][j],triangle[i+1][j+1]);
             }
         }
-        return ans;
+        return triangle[0][0];
     }
 };
