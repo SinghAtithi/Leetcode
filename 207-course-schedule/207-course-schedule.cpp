f23b746d69873fa0e6d08f38ec89226080bce7a5
Atithi Kumar singh <mails4rockstararyan@gmail.com>
2022-06-14 12:50:46 +0530

    Time: 24 ms (79.56%), Space: 13.4 MB (75.49%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        int n = numCourses;
+        if(numCourses == 1) return true;
+        vector<vector<int>> graph(n);
+        vector<int> indeg(n,0);
+        for(int i=0;i<prerequisites.size();i++){
+            graph[prerequisites[i][1]].push_back(prerequisites[i][0]);
+            indeg[prerequisites[i][0]]++;
+        }
+        queue<int> pq;
+        for(int i=0;i<n;i++){
+            if(indeg[i] ==0){
+                pq.push(i);
+            }
+        }
+        int cnt = 0;
+        vector<int> ans;
+        while(!pq.empty()){
+            cnt++;
+            int x =pq.front();
+            pq.pop();
+            ans.push_back(x);
+            for(auto it:graph[x]){
+                indeg[it]--;
+                if(indeg[it] == 0){
+                    pq.push(it);
+                }
+            }
+        }
+        return (ans.size() == numCourses);
+
+    }
+};
