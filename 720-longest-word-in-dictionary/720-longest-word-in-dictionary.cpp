fd689e653e0659d99e4adf1dd4c8c9f496ea1dd6
Atithi Kumar singh <mails4rockstararyan@gmail.com>
2022-06-14 19:42:53 +0530

    Time: 67 ms (63.97%), Space: 16.9 MB (96.61%) - LeetHub

---
+++ 720-longest-word-in-dictionary/720-longest-word-in-dictionary.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    
+    static bool comp(string &a, string &b){
+        if(a.size() == b.size())
+            return a > b;
+        return a.size() < b.size();
+    }
+    
+    string longestWord(vector<string>& words) {
+        
+        string ans = "";
+        set<string>st;
+        sort(words.begin(),words.end(),comp);
+        
+        st.insert("");
+        
+        int len = words.size();
+        
+        for(int i =  0; i < len; i++){
+            char x = words[i].back();
+            words[i].pop_back();
+            // cout<<words[i] <<"\n";
+            if(st.find(words[i]) != st.end()){
+                words[i] += x;
+                ans = words[i];
+                st.insert(words[i]);
+            }
+        }
+        
+        return ans;
+    }
+};
