65e9f742274c9e60b84869544b1b4b9e087ba301
Atithi Kumar singh <mails4rockstararyan@gmail.com>
2022-06-14 12:51:39 +0530

    Time: 24 ms (79.56%), Space: 13.4 MB (75.49%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        
+        ios_base::sync_with_stdio(false); cin.tie(NULL);cout.tie(NULL);
+        
         int n = numCourses;
         if(numCourses == 1) return true;
         vector<vector<int>> graph(n);
