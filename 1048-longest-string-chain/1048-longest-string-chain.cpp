5e8add12032061f1b6a7312f838faa357bca46dd
Atithi Kumar singh <mails4rockstararyan@gmail.com>
2022-06-15 12:22:42 +0530

    Time: 158 ms (43.33%), Space: 13.2 MB (83.98%) - LeetHub

---
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    
+    static bool comp(string &a, string &b){
+        return a.size() < b.size();
+    }
+    
+    bool check(string &s, string &t){
+        
+        int cnt = 0;
+        int n = s.size();
+        int m = t.size();
+        
+        if((m - n) != 1)
+            return false;
+        
+        for(int i = 0; i < n; i++){
+            if(s[i]!=t[i + cnt]){
+                cnt++;
+                i--;
+                if(cnt > 1)
+                    return false;
+            }
+        }
+        return true;
+    }
+    
+    
+    int longestStrChain(vector<string>& words) {
+        
+        int len = words.size();
+        vector<int>dp(len,0);
+        int ans = INT_MIN;
+        
+        sort(words.begin(), words.end(), comp);
+        
+        for(int i = 0; i < len; i++){
+            dp[i] = max(dp[i] , 1);
+            for(int j = i + 1; j < len; j++){
+                if(check(words[i], words[j])){
+                    dp[j] = max(dp[j], dp[i] + 1);
+                }
+            }
+            ans = max(ans, dp[i]);
+        }
+        return ans;
+    }
+};
