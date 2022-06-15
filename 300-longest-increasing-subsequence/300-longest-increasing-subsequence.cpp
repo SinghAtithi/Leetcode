d4ff86f130c3f1e4b1af9dfcaf23721ebb04efef
Atithi Kumar singh <mails4rockstararyan@gmail.com>
2022-06-15 14:21:05 +0530

    Time: 11 ms (86.77%), Space: 12.9 MB (11.54%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);cout.tie(NULL);
+        
         int INF = 1e9 + 7;
         set<int>st;
         st.insert(INF);
