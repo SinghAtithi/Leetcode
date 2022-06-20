974e460d5bd8d6ae0530081439984149b55de694
Atithi Kumar singh <mails4rockstararyan@gmail.com>
2022-06-20 12:35:51 +0530

    Time: 114 ms (53.47%), Space: 18.4 MB (67.82%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -5,40 +5,27 @@ public:
         return s.size() < ss.size();
     }
     
-    bool check(string s, string ss){
-        while(s.size()){
-            if(s.back() == ss.back()){
-                s.pop_back();
-                ss.pop_back();
-            }
-            else{
-                return false;
-            }
-        }
-        return true;
-    }
-    
     int minimumLengthEncoding(vector<string>& words) {
         
-        int ans= 0;
+        set<string>st;
         int len = words.size();
+        int ans = 0;
+        
         sort(words.begin(), words.end(), comp);
-                cout<<0;
-
+        
         for(int i = 0; i < len; i++){
-            ans++;
-            ans += words[i].size();
-            for(int j = i + 1; j < len;j++){
-                if(check(words[i], words[j])){
-                    ans--;
-                    ans -= words[i].size();
-                    // cout<<words[i]<<" "<<words[j]<<"\n";
-                    break;
+            ans += words[i].size() + 1;
+            string s = "";
+            for(int j = words[i].size() - 1; j >= 0; j--){
+                s += words[i][j];
+                if(st.find(s) != st.end()){
+                    st.erase(s);
+                    ans -= s.size() + 1;
                 }
             }
+            st.insert(s);
+            // cout<<ans<<"\n";
         }
-        
-        
         return ans;
     }
 };
