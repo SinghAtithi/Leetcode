7dbd46cd1ffc68abb9ffa673e6236a9ec2723471
Atithi Kumar singh <mails4rockstararyan@gmail.com>
2022-06-22 00:41:57 +0530

    Time: 18 ms (21.47%), Space: 9.6 MB (55.99%) - LeetHub

---
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        int ans = 0;
+        int t = 1;
+        int len = nums.size();
+        
+        for(int i = 0; i < 32; i++){
+            int cnt = 0;
+            for(int &x: nums){
+                if(((x>>i) & 1) != 0){
+                    cnt++;
+                }
+            }
+            if(cnt % 3 != 0){
+                ans += (1<<i);
+            }
+        }
+        return ans;
+    }
+};
