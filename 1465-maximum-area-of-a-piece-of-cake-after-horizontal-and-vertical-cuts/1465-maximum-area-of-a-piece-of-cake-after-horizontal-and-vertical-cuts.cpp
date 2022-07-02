4707007f67ac04a8a5dfb52d888e04ceb4af4215
Atithi Kumar singh <mails4rockstararyan@gmail.com>
2022-07-02 11:13:46 +0530

    Time: 108 ms (42.45%), Space: 32.4 MB (40.04%) - LeetHub

---
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    
+    int MOD = 1000000007;
+    
+    long long maxDist(vector<int>& v, int last){
+        v.push_back(last);
+        sort(v.begin(), v.end());
+        int ans = v[0];
+        for(int i = 0; i < v.size() - 1; i++){
+            ans = max(ans , v[i + 1] - v[i]);
+        }
+        return ans % MOD;
+    }
+    
+    int maxArea(int h, int w, vector<int>& horizontalCuts, vector<int>& verticalCuts) {
+        
+        long long maxH = maxDist(horizontalCuts, h);
+        long long maxW = maxDist(verticalCuts, w);
+        return (maxH * maxW) % MOD;
+        
+    }
+};
