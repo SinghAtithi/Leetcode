8e4d24447de68165839e5021a3dfc302aea189f8
Atithi Kumar singh <mails4rockstararyan@gmail.com>
2022-07-03 11:08:24 +0530

    Time: 4 ms (56.21%), Space: 7.2 MB (31.92%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -3,11 +3,10 @@ public:
     int wiggleMaxLength(vector<int>& nums) {
         
         int len = nums.size();
-        vector<int>dpp(len, 0);
         vector<int>dp(len, 0);
 
         
-        dp[0] = dpp[0] = 1;
+        dp[0] = 1;
         bool flag = true;
         
         for(int i = 1; i < len; i++){
@@ -26,26 +25,27 @@ public:
             }
         }
         
-        
+        int x = dp[len - 1];
         flag = false;
+        dp[0] = 1;
         
         for(int i = 1; i < len; i++){
-            dpp[i] = dpp[i - 1];
+            dp[i] = dp[i - 1];
             if(flag){
                 if(nums[i] > nums[i - 1]){
-                    dpp[i] = dpp[i - 1] + 1;
+                    dp[i] = dp[i - 1] + 1;
                     flag = !flag;
                 }
             }
             else{
                 if(nums[i] < nums[i - 1]){
-                    dpp[i] = dpp[i - 1] + 1;
+                    dp[i] = dp[i - 1] + 1;
                     flag = !flag;
                 }
             }
         }
         
         
-        return max(dp[len - 1], dpp[len - 1]);
+        return max(x, dp[len - 1]);
     }
 };
